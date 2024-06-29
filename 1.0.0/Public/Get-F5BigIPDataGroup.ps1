de39180fd0c55be0ade2b45918b3e67ae5f588bc
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2024-06-29 00:13:04 -0400

    First Commit

---
+++ 1.0.0/Public/Get-F5BigIPDataGroup.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+    Connects to F5 and outputs list of records for a specific data group
+
+.EXAMPLE 
+    Get-F5BigIPDataGroup -F5BigIpAddress "XXXX.xxx.com" -Partition "common" -DataGroupName "DataGroupName" -F5Creds "CredObject"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 6/14/2024
+
+#>
+Function Get-F5BigIPDataGroup {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your F5 BigIP Address.")]
+        [string[]]$F5BigIpAddress,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Partition.")]
+        [string[]]$Partition,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Data Group Name.")]
+        [string[]]$DataGroupName,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your F5 Creds.")]
+        [System.Management.Automation.PSCredential]$F5Creds
+    )
+	
+	$Uri = "https://$($F5BigIpAddress)/mgmt/tm/ltm/data-group/internal/~$($Partition)~$($DataGroupName)"
+
+    $Response = Invoke-RestMethod -Uri $Uri -Method Get -Credential $F5Creds -ContentType "application/json"
+    Write-Output $Response
+}
---
+++ 1.0.0/Public/Update-F5BigIPDataGroupRecord.ps1
@@ -0,0 +1,54 @@
+﻿<#
+.Synopsis
+    Connects to F5 and updates specific Record
+
+.EXAMPLE 
+    Update-F5BigIPDataGroupRecord -F5BigIpAddress "XXXX.xxx.com" -Partition "common" -DataGroupName "DataGroupName" -DataGroupRecordName "DataGroupRecordName" -DataGroupRecordData "DataGroupRecordData" -F5Creds "CredObject"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 6/14/2024
+
+#>
+Function Update-F5BigIPDataGroupRecord {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your F5 BigIP Address.")]
+        [string[]]$F5BigIpAddress,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Partition.")]
+        [string[]]$Partition,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Data Group Name.")]
+        [string[]]$DataGroupName,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Data Group Record Name.")]
+        [string[]]$DataGroupRecordName,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Data Group Record Data.")]
+        [string[]]$DataGroupRecordData,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your F5 Creds.")]
+        [System.Management.Automation.PSCredential]$F5Creds
+    )
+
+    $Body = @{name="$DataGroupName"}
+    $JsonBody = ConvertTo-Json $Body
+	
+    $Uri = "https://$($F5BigIpAddress)/mgmt/tm/ltm/data-group/internal/~$($Partition)~$($DataGroupName)?options=records+modify+{$($DataGroupRecordName){data+`"$($DataGroupRecordData)`"}}"
+
+    $Response = Invoke-RestMethod -Uri $Uri -Method Patch -Credential $F5Creds -ContentType "application/json" -Body $JsonBody
+    Write-Output $Response
+}
